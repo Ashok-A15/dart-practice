8c2e3d5f53fcf3a6f6ffc5fabc3e2bd2437e9ba9
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-05-06 20:10:06 +0530

    Update control flow if statement.dart

--- control flow if statement.dart
+++ control flow if statement.dart
@@ -9,3 +9,22 @@ void main() {
   }
   }
   
+
+
+
+
+
+
+
+// we use == relational operator
+void main() {
+   int age =21;
+  if(age==21){
+    print("ADULT");
+  }else if(age>=18){
+      print("ADULT");
+  }else if(age==18){
+    print("CHILD");
+  }
+  }
+  
